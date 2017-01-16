fc83dc0a2d3f8bff084c8e388f9cfb8f3977041d
Jakub Scholz <jakub@scholz.cz>
2017-01-16 10:54:54 +0100

    Add Travis CI integration

--- Dockerfile/create.sh
+++ Dockerfile/create.sh
@@ -4,12 +4,12 @@
 cp -r -v ./target/vertx-amqp-bridge-1.0-SNAPSHOT/vertx-amqp-bridge-1.0-SNAPSHOT ./Dockerfile/vertx-amqp-bridge-1.0-SNAPSHOT
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1} ./Dockerfile/
-docker tag scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1}
-docker push scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1}
-docker tag scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_BRANCH}
-docker push scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_BRANCH}
-docker tag scholzj/livescore-demo-vertx-amqp-bridge:${CIRCLE_SHA1} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:latest
+docker build -t scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT} ./Dockerfile/
+docker tag scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT}
+docker push scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT}
+docker tag scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_BRANCH}
+docker push scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_BRANCH}
+docker tag scholzj/livescore-demo-vertx-amqp-bridge:${TRAVIS_COMMIT} docker.io/scholzj/livescore-demo-vertx-amqp-bridge:latest
 docker push scholzj/livescore-demo-vertx-amqp-bridge:latest
 
 # Delete the binaries
